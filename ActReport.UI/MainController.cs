d957451f6e9679e83af2786371f50181167d19db
j07122000 <janine.hoellhuber@gmx.at>
2020-10-26 14:39:25 +0100

    Add project files.

---
+++ ActReport.UI/MainController.cs
@@ -0,0 +1,28 @@
+﻿using ActReport.ViewModel;
+using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActReport.UI
+{
+    class MainController :IController
+    {
+        public void ShowWindow(BaseViewModel viewModel)
+        {
+            Window window = viewModel switch
+            {
+                null => throw new ArgumentException(nameof(viewModel)),
+
+                EmployeeViewModel _ => new MainWindow(),
+                ActivityViewModel _ => new ActivityWindow(),
+
+                _ => throw new InvalidOperationException($"Unkonwn ViewModel of type '{viewModel}'"),
+            };
+
+            window.DataContext = viewModel;
+
+            window.ShowDialog();
+        }
+    }
+}
